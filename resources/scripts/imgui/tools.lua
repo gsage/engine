e35d6957a0099d4ca39bf7fcf8125e5c6f8e5bd8
Artem Chernyshev <artem.chernyshev@emc.com>
2019-02-21 09:26:05 +0300

    [#164035605] Display tools panel statically, instead of docking

--- resources/scripts/imgui/tools.lua
+++ resources/scripts/imgui/tools.lua
@@ -4,15 +4,21 @@ local icons = require 'imgui.icons'
 local lm = require 'lib.locales'
 
 -- editor tools
-ToolsView = class(ImguiWindow, function(self, docked, open)
-  ImguiWindow.init(self, "tools", docked, open)
+ToolsView = class(ImguiWindow, function(self, open, positioner)
+  ImguiWindow.init(self, "tools", false, open)
   self.icon = icons.dehaze
-  self.flags = ImGuiWindowFlags_NoScrollWithMouse + ImGuiWindowFlags_NoScrollbar
+  self.flags = ImGuiWindowFlags_NoScrollWithMouse + ImGuiWindowFlags_NoScrollbar + ImGuiWindowFlags_NoTitleBar + ImGuiWindowFlags_NoResize + ImGuiWindowFlags_NoMove
+  self.positioner = positioner
 end)
 
 -- render tools
 function ToolsView:__call()
+  local x, y, width, height = self.positioner()
+  imgui.SetNextWindowPos(x, y)
+  imgui.SetNextWindowSize(width, height)
+
   if self:imguiBegin() then
+
     -- new project
     imgui.Button(icons.insert_drive_file)
     if imgui.IsItemHovered() then
